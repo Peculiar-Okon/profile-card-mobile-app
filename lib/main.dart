da41085059dc8236330635edbead1529939b2c1a
peculiar okon <peculiargirl4real@gmail.com>
2025-09-23 14:03:58 +0100

    wriring the main

--- lib/main.dart
+++ lib/main.dart
@@ -1576,4 +1576,32 @@
 //   }
 // }
 
+import 'package:flutter/material.dart';
+import 'views/home_view.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.dark(
+    surface: Colors.grey.shade900,
+    primary: Colors.grey.shade600,
+    secondary: const Color.fromARGB(255, 55, 55, 55),
+    inversePrimary: Colors.grey.shade300,
+  ),
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: darkMode,
+      home: const HomeView(),
+    );
+  }
+}
